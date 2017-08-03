ed415b9999c035295f9c31fb6908928a87023d87
Fantomas42 <fantomas42@gmail.com>
2017-08-03 10:41:17 +0200

    Option to bypass lock

--- aluzine/scripts/out.py
+++ aluzine/scripts/out.py
@@ -14,12 +14,19 @@ def cmdline():
         type=int,
         help='Range of minutes before pinging')
 
+    parser.add_argument(
+        '--no-lock',
+        action='store_true',
+        help='By pass lock checking')
+
     args = parser.parse_args()
+    no_lock = args.no_lock
     random_range = args.random_range
 
-    lock = Lock()
-    if not lock.exist():
-        return False
+    if not no_lock:
+        lock = Lock()
+        if not lock.exist():
+            return False
 
     if random_range:
         sleep(randint(0, random_range) * 60)
